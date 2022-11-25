81c5976af51f9607a8bc9f88da4ec826ce37765b
fks <fuku02@gmail.com>
2022-11-25 16:08:43 +0900

    add allday

--- lib/src/components/days_row/days_row.dart
+++ lib/src/components/days_row/days_row.dart
@@ -57,7 +57,7 @@ class _DayCell extends HookConsumerWidget {
     //ソート（繰り返しの場合、日付を比較しない）
     eventsOnTheDate?.sort((a, b) {
       if (a.allday != b.allday) {
-        return a.allday.toString().compareTo(b.allday.toString());
+        return b.allday.toString().compareTo(a.allday.toString());
       } else {
         var _a = DateTime(date.year, date.month, date.day, a.start.hour, a.start.minute, a.start.second, a.start.millisecond, a.start.microsecond).toLocal();
         var _b = DateTime(date.year, date.month, date.day, b.start.hour, b.start.minute, b.start.second, b.start.millisecond, b.start.microsecond).toLocal();
